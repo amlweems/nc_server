c999fa94466712bb7a6eea0517817d55fe6f5629
Anthony Weems <amlweems@gmail.com>
2013-09-29 01:30:33 -0500

    4 spaces are better than tabs

--- nc_server.py
+++ nc_server.py
@@ -2,8 +2,8 @@
 import socket
 
 def send(data):
-	print data
-	return data.encode('rot13')
+    print data
+    return data.encode('rot13')
 
 HOST = '0.0.0.0'
 PORT = 7776
@@ -11,21 +11,21 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
 try:
-	print "Running server on port", PORT
-	print "Press ^C to close the socket"
-	while True:
-		conn, addr = s.accept()
-		print 'Connected by', ':'.join([str(i) for i in addr])
-		try:
-			while True:
-			    data = conn.recv(1024)
-			    if not data: break
-			    conn.send(send(data))
-		except Exception as e:
-			print e
-		finally:
-			conn.close()
+    print "Running server on port", PORT
+    print "Press ^C to close the socket"
+    while True:
+        conn, addr = s.accept()
+        print 'Connected by', ':'.join([str(i) for i in addr])
+        try:
+            while True:
+                data = conn.recv(1024)
+                if not data: break
+                conn.send(send(data))
+        except Exception as e:
+            print e
+        finally:
+            conn.close()
 except:
-	pass
+    pass
 finally:
-		s.close()
+        s.close()
